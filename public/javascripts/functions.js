0c2133a26af9165261b546789458a72e7cbaf60d
Jennifer Cui <jcui@Jennifers-MacBook.local>
2015-05-15 11:52:18 -0400

    switch days working, up to adding map features

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,14 +1,11 @@
 $(document).ready(function( ){
-	var dayVal = 1;
-	var currentDay = dayVal;
+	var currentDay = 1;
 	var Day = function(){
 		this.hotels = [];
 		this.restaurants = [];
 		this.toDo = [];
 	};
-
 	var daysArray = [new Day()];
-	// console.log(daysArray);
 
 
 
@@ -36,18 +33,17 @@ $(document).ready(function( ){
 	})
 
 // removing from itinerary
-
-
 //select parent div, click assigned to button's id, remove parent and remove self
 	$('#addedHotel').delegate('#removeH', 'click', function () {
 	    $(this).parent().remove();
+
 	    var nameArr = $(this).parent().html().split("");
 	    var slicedName = nameArr.slice(0, nameArr.indexOf("<")-2).join('');
-	    for (var i=0; i<daysArray[dayVal-1].hotels.length; i++){
-	    	// console.log(dayVal);
-	    	if(daysArray[dayVal-1].hotels[i] === slicedName){
-	    		console.log(daysArray[dayVal-1].hotels[i]);
-	    		daysArray[dayVal-1].hotels.splice(i,1);
+
+	    for (var i=0; i<daysArray[currentDay-1].hotels.length; i++){
+	    	if(daysArray[currentDay-1].hotels[i] === slicedName){
+	    		console.log(daysArray[currentDay-1].hotels[i]);
+	    		daysArray[currentDay-1].hotels.splice(i,1);
 	    		return;
 	    	}
 	    }
@@ -59,12 +55,14 @@ $(document).ready(function( ){
 
 	$('#addedRest').delegate('#removeR', 'click', function () {
 	    $(this).parent().remove();
+
 	    var nameArr = $(this).parent().html().split("");
 	    var slicedName = nameArr.slice(0, nameArr.indexOf("<")-2).join('');
-	    for (var i=0; i<daysArray[dayVal-1].restaurants.length; i++){
-	    	if(daysArray[dayVal-1].restaurants[i] === slicedName){
-	    		daysArray[dayVal-1].restaurants.splice(i,1);
-	    		console.log(daysArray[dayVal-1].restaurants);
+
+	    for (var i=0; i<daysArray[currentDay-1].restaurants.length; i++){
+	    	if(daysArray[currentDay-1].restaurants[i] === slicedName){
+	    		daysArray[currentDay-1].restaurants.splice(i,1);
+	    		console.log(daysArray[currentDay-1].restaurants);
 	    		return;
 	    	}
 	    }
@@ -73,63 +71,55 @@ $(document).ready(function( ){
 
 	$('#addedToDo').delegate('#removeT', 'click', function () {
 	    $(this).parent().remove();
+
 	    var nameArr = $(this).parent().html().split("");
 	    var slicedName = nameArr.slice(0, nameArr.indexOf("<")-2).join('');
-	    for (var i=0; i<daysArray[dayVal-1].toDo.length; i++){
-	    	if(daysArray[dayVal-1].toDo[i] === slicedName){
-	    		daysArray[dayVal-1].toDo.splice(i,1);
-	    		console.log(daysArray[dayVal-1].toDo);
+
+	    for (var i=0; i<daysArray[currentDay-1].toDo.length; i++){
+	    	if(daysArray[currentDay-1].toDo[i] === slicedName){
+	    		daysArray[currentDay-1].toDo.splice(i,1);
+	    		console.log(daysArray[currentDay-1].toDo);
 	    		return;
 	    	}
 	    }
 	    $(this).remove();
 	});
 
+
+// switching days
 $('#days').delegate('.buttonDays', 'click', function(){
-	// console.log("clicked!");
-		// $('#day-title').first().text("Day "+dayNumber);
-		dayVal = +$(this).text();
-		console.log(dayVal);
-		$("#selectedDay").text("Day " + dayVal);
+		currentDay = +$(this).text();
+		console.log(currentDay);
+		$("#selectedDay").text("Day " + currentDay);
 	$('#addedHotel, #addedRest, #addedToDo').empty();
-	for (var i=0; i<daysArray[dayVal-1].hotels.length; i++){ //bring back hotel
-		$("#addedHotel").append("<p>" + daysArray[dayVal-1].hotels[i] + "  " + "<span id= 'removeH'><button class= 'btn btn-xs btn-danger remove btn-circle'>x</button></span></p>");
+	for (var i=0; i<daysArray[currentDay-1].hotels.length; i++){ //bring back hotel
+		$("#addedHotel").append("<p>" + daysArray[currentDay-1].hotels[i] + "  " + "<span id= 'removeH'><button class= 'btn btn-xs btn-danger remove btn-circle'>x</button></span></p>");
 	}
-	for (var i=0; i<daysArray[dayVal-1].restaurants.length; i++){// bring back restaurant
-		console.log(daysArray[dayVal-1].restaurants[i]);
-		$("#addedRest").append("<p>" + daysArray[dayVal-1].restaurants[i] + "  " + "<span id= 'removeR'><button class= 'btn btn-xs btn-danger remove btn-circle'>x</button></span></p>");
+	for (var i=0; i<daysArray[currentDay-1].restaurants.length; i++){// bring back restaurant
+		console.log(daysArray[currentDay-1].restaurants[i]);
+		$("#addedRest").append("<p>" + daysArray[currentDay-1].restaurants[i] + "  " + "<span id= 'removeR'><button class= 'btn btn-xs btn-danger remove btn-circle'>x</button></span></p>");
 	}
-	for (var i=0; i<daysArray[dayVal-1].toDo.length; i++){ //bring back todo
-		console.log(daysArray[dayVal-1].toDo[i]);
-		$("#addedToDo").append("<p>" + daysArray[dayVal-1].toDo[i] + "  " + "<span id= 'removeT'><button class= 'btn btn-xs btn-danger remove btn-circle'>x</button></span></p>");
+	for (var i=0; i<daysArray[currentDay-1].toDo.length; i++){ //bring back todo
+		console.log(daysArray[currentDay-1].toDo[i]);
+		$("#addedToDo").append("<p>" + daysArray[currentDay-1].toDo[i] + "  " + "<span id= 'removeT'><button class= 'btn btn-xs btn-danger remove btn-circle'>x</button></span></p>");
 	}
 
 })
 
 
-
+//add a day
 	$('#add-day').click(function(){
 		daysArray.push(new Day());
 		currentDay++;
 		$('<button type="button" class="btn btn-circle buttonDays" id= "Day '+currentDay+'">'+ currentDay+'</button>').insertBefore($(this));
 	})
 
-	function reNumberArr(arr, index){
-    var splicedNum = arr.splice(index, 1);
-    var splicedArr = arr;
-    
-    for (var i=index; i<splicedArr.length; i++){
-        splicedArr[i] = splicedNum[0];
-        splicedNum[0]++;
-    }
-    return splicedArr;
-}
-
-	$('#remove-day').click(function(){
-		daysArray.splice(dayVal-1, 1);
+//remove a day
+	$('#day-title').delegate('#remove-day', 'click', function () {
+		daysArray.splice(currentDay-1, 1);
 		console.log(daysArray);
-
-	})
+		$("#add-day").prev().remove();
+	});
 
 
 });
